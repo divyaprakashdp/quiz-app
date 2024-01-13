0962adddf1e6623a59eeed3ea5c91d2c505a65a9
divyaprakash <d.prakash1506@gmail.com>
2024-01-13 12:19:42 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-screen w-full">
+      <h1 className="font-heading text-6xl">Quiz App</h1>
+      <p>Answer following for fun</p>
+    </div>
+  );
+}
+
+export default App;
