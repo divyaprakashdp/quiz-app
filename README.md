a6d5011a7211d4644e299b12513a464e24a6c973
divyaprakash <d.prakash1506@gmail.com>
2024-01-16 00:05:45 +0530

    mvp commit

--- README.md
+++ README.md
@@ -1,8 +1,69 @@
-# React + Vite
+# QuizApp Readme
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+QuizApp is a modern quiz website built with ReactJS, Vite, and Tailwind CSS. It utilizes a Quiz API to provide a wide range of quizzes on various topics. Whether you want to test your knowledge in science, history, pop culture, or any other category, QuizApp has got you covered.
 
-Currently, two official plugins are available:
+## Table of Contents
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- [QuizApp Readme](#quizapp-readme)
+  - [Table of Contents](#table-of-contents)
+  - [Getting Started](#getting-started)
+    - [Prerequisites](#prerequisites)
+    - [Installation](#installation)
+    - [Install dependencies:](#install-dependencies)
+    - [Running the App](#running-the-app)
+    - [Tech Stack](#tech-stack)
+    - [Features](#features)
+    - [API Integration](#api-integration)
+
+## Getting Started
+
+### Prerequisites
+
+Make sure you have the following installed on your machine:
+
+- Node.js: [Download Here](https://nodejs.org/)
+- npm (Node Package Manager): This comes with Node.js installation.
+
+### Installation
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/your-username/quizzy-app.git
+```
+
+2. Change into the project directory:
+```bash
+cd quiz-app
+```
+
+### Install dependencies:
+``` bash
+npm install
+```
+
+### Running the App
+To start the development server, run:
+``` bash
+npm run dev
+```
+
+
+This will launch the application on http://localhost:5137. Open your browser and navigate to this URL to explore QuizApp.
+
+### Tech Stack
+- ```ReactJS```: A JavaScript library for building user interfaces.
+- ```Vite```: A fast, opinionated frontend build tool that aims to provide a seamless development experience.
+- ```Tailwind CSS```: A utility-first CSS framework for rapidly building custom designs.
+- ```Quiz API```: An external API providing a variety of quizzes.
+
+### Features
+- Choose from a diverse range of quizzes.
+- Track your quiz progress and scores.
+- Responsive design for a seamless experience on different devices.
+- Engaging user interface with smooth transitions and animations.
+
+### API Integration
+QuizApp integrates with an external Quiz API to fetch quiz questions and categories dynamically. The services folder contains modules responsible for handling API requests.
+
+To configure the API endpoint or API key, refer to the relevant service file.
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,165 @@
+import { useEffect } from "react";
 import "./App.css";
+import { useReducer } from "react";
+
+import PropTypes from "prop-types";
+
+const intialState = {
+  questions: [],
+  allOptions: [],
+  correctOption: null,
+  //status -> loading, error, ready, active, finished
+  status: "loading",
+  qindex: 0,
+  answer: null,
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return {
+        ...state,
+        questions: action.payload,
+        allOptions: [
+          ...action.payload?.[state.qindex]?.incorrect_answers,
+          action.payload?.[state.qindex]?.correct_answer,
+        ].sort(() => Math.random() - 0.6),
+        correctOption: action.payload?.[state.qindex]?.correct_answer,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    case "start":
+      return {
+        ...state,
+        status: "active",
+      };
+    case "answered":
+      return {
+        ...state,
+        answer: action.payload,
+      };
+    default:
+      throw new Error("Action unknown");
+  }
+}
 
 function App() {
+  const [
+    { questions, status, qindex, answer, allOptions, correctOption },
+    dispatch,
+  ] = useReducer(reducer, intialState);
+  useEffect(function () {
+    fetch(
+      "https://opentdb.com/api.php?amount=10&category=23&difficulty=easy&type=multiple"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.results?.[0]);
+        dispatch({
+          type: "dataRecieved",
+          payload: data.results,
+        });
+      })
+      .catch(() =>
+        dispatch({
+          type: "dataFailed",
+        })
+      );
+  }, []);
+
   return (
     <div className="h-screen w-full">
       <h1 className="font-heading text-6xl">Quiz App</h1>
-      <p>Answer following for fun</p>
+      <p>Unleash Your Inner Smarty Pants</p>
+      <div>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen
+            category={questions?.[0]?.category}
+            dispatch={dispatch}
+            // noOfQuestions={questions.length}
+          />
+        )}
+        {status === "active" && (
+          <Questions
+            question={questions?.[qindex]?.question}
+            index={qindex}
+            options={allOptions}
+            correctOption={correctOption}
+            answer={answer}
+            dispatch={dispatch}
+          />
+        )}
+      </div>
     </div>
   );
 }
 
+function Loader() {
+  return <div>Loading...</div>;
+}
+
+function Error() {
+  return <div>Error fetching the questions!</div>;
+}
+
+function StartScreen({ category, dispatch }) {
+  return (
+    <div>
+      <h2>{`Welcome to the quiz on ${category}!`}</h2>
+      <p>{`10 questions to test your mastery`}</p>
+      <button onClick={() => dispatch({ type: "start" })}>Let's Start</button>
+    </div>
+  );
+}
+
+StartScreen.propTypes = {
+  category: PropTypes.string,
+  noOfQuestions: PropTypes.number,
+  dispatch: PropTypes.func,
+};
+
+function Questions({ question, options, correctOption, answer, dispatch }) {
+  let correctCss = (index) =>
+    answer !== null
+      ? index === options.indexOf(correctOption)
+        ? "bg-green-400"
+        : "bg-red-400"
+      : "";
+
+  return (
+    <div>
+      <h2>QUESTIONS</h2>
+      <div>
+        <p>{`${question}`}</p>
+        <ul>
+          <li>
+            {options.map((option, index) => (
+              <button
+                key={option}
+                disabled={answer != null}
+                className={`bg-gray-500 w-[40%] m-4 py-2 rounded-lg gap-4 text-white ${
+                  answer === index ? "bg-gray-700" : ""
+                } ${correctCss(index)}`}
+                onClick={() => dispatch({ type: "answered", payload: index })}
+              >{`${option}`}</button>
+            ))}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+Questions.propTypes = {
+  question: PropTypes.string,
+  options: PropTypes.array,
+  answer: PropTypes.number,
+  dispatch: PropTypes.func,
+  correctOption: PropTypes.string,
+};
 export default App;
